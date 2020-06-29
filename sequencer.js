b9e90a5583c23e15cf383a8d5a8742ddf9aef220
MazeArt <fcoarturo.marshall@gmail.com>
2020-06-29 04:50:42 -0400

    test audioload

--- sequencer.js
+++ sequencer.js
@@ -1,88 +1,87 @@
 
-let pattern_mapy= {
-    kick_arr: [1,0,0,0,0,0,0,1],
-    snare_arr: [0,0,0,0,1,0,0,0],
-    hats_arr: [1,1,1,1,1,1,1,1]
+let pattern_mapy = {
+  kick_arr: [1, 0, 0, 0, 0, 0, 0, 1],
+  snare_arr: [0, 0, 0, 0, 1, 0, 0, 0],
+  hats_arr: [1, 1, 1, 1, 1, 1, 1, 1]
 }
 
-function play_moi(){
-    sequencer(pattern_mapy);
+function play_moi() {
+  sequencer(pattern_mapy);
 }
 
-document.querySelector('button').addEventListener('click', async () => {
-	await Tone.start()
-	console.log('audio is ready')
-})
+// document.querySelector('#init_button').addEventListener('click', async () => {
+//   await Tone.start()
+//   console.log('audio is ready')
+// })
 
-let isPlaying=false
+let isPlaying = false
 
 
-function sequencer(pattern_map){
+function sequencer(pattern_map) {
   //https://github.com/Tonejs/Tone.js#starting-audio
-  let kickBuff = new Tone.Buffer('sounds/kick.mp3');
+
+
+   let kickBuff = new Tone.Buffer('sounds/kick.mp3');
   let snare_accBuff= new Tone.Buffer('sounds/snare_acc.mp3');
   let snareBuff= new Tone.Buffer('sounds/snare.mp3');
   let hhBuff= new Tone.Buffer('sounds/hats_closed.mp3');
+
   
- let kick_tone = new Tone.Player(kickBuff).toMaster();
+   let kick_tone = new Tone.Player(kickBuff).toMaster();
    let snare_acc_tone = new Tone.Player(snare_accBuff).toMaster();
    let snare_tone = new Tone.Player(snareBuff).toMaster();
    let hh_tone = new Tone.Player(hhBuff).toMaster();
+  
+  
+
+  let index = 0;
+
+  Tone.Transport.scheduleRepeat(time => {
+    repeat(time);
+  }, "16n");
+  Tone.Transport.bpm.value = 70
+  Tone.Transport.start();
 
 
-  //  let kick_tone = new Tone.Player('sounds/kick.mp3').toMaster();
-  //  let snare_acc_tone = new Tone.Player('sounds/snare_acc.mp3').toMaster();
-  //  let snare_tone = new Tone.Player('sounds/snare.mp3').toMaster();
-  //  let hh_tone = new Tone.Player('sounds/hats_closed.mp3').toMaster();
-    //const hh_2 = new Tone.Player('./sounds/hats_closed.mp3').toMaster();
-    let index=0;
 
-    Tone.Transport.scheduleRepeat(time => {
-        repeat(time);
-      }, "16n");
-    Tone.Transport.bpm.value = 70
-    Tone.Transport.start();
-    
-    
-
-    function repeat(time){
-        if (Tone.context.state !== 'running') {
-            Tone.context.resume();
-        }
-        isPlaying=true
-        let step = index % 16;
-        let kickInput = pattern_map.kick_arr[step];
-        console.log(step);
-        if(kickInput){
-            kick_tone.start(time);
-            console.log(step," kick!")
-        }
-        if(pattern_map.snare_arr[step]){
-          snare_tone.volume.value = -22
-          snare_tone.start(time);
-            console.log(step," snare!")
-        }
-        if(pattern_map.snare_acc_arr[step]){
-           // snare_acc.volume.value = 0
-           snare_acc_tone.start(time);
-            console.log(step," snare_acc!")
-        }
-        if(pattern_map.hats_arr[step]){ 
-           hh_tone.volume.value = -14
-          hh_tone.start(time);
-    
-            console.log(step," hh!")
-        }
-        index++;
+  function repeat(time) {
+    if (Tone.context.state !== 'running') {
+      Tone.context.resume();
     }
+    isPlaying = true
+    let step = index % 16;
+    let kickInput = pattern_map.kick_arr[step];
+    console.log(step);
+    if (kickInput) {
+      kick_tone.start(time);
+      console.log(step, " kick!")
+    }
+    if (pattern_map.snare_arr[step]) {
+      snare_tone.volume.value = -22
+      snare_tone.start(time);
+      console.log(step, " snare!")
+    }
+    if (pattern_map.snare_acc_arr[step]) {
+      // snare_acc.volume.value = 0
+      snare_acc_tone.start(time);
+      console.log(step, " snare_acc!")
+    }
+    if (pattern_map.hats_arr[step]) {
+      hh_tone.volume.value = -14
+      hh_tone.start(time);
+
+      console.log(step, " hh!")
+    }
+    index++;
+  }
 
 }
 
-function stopSeq(){
+function stopSeq() {
 
 
 
-}  
+}
 
 
 // :::::::: P5 Solution
